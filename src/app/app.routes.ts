45935b128f938e193892fd7c7b4bb704024f0b29
Developer <developer@neomotors.com>
2026-06-22 16:38:42 -0600

    feat(empresas): add CRUD Empresas UI with list and form pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,11 @@ export const routes: Routes = [
       import('./features/auth/auth.routes').then((r) => r.authRoutes),
   },
   {
+    path: 'companies',
+    loadChildren: () =>
+      import('./features/companies/companies.routes').then((r) => r.companyRoutes),
+  },
+  {
     path: '',
     redirectTo: '/auth/login',
     pathMatch: 'full',
